c0d635e469c757ae096daefc34e0298ea9a9c348
adamrossbenton <adamrossbenton@gmail.com>
2022-02-07 16:21:25 -0800

    index/show are set up, needs about/contact and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import {Nav, Navbar, NavDropdown} from "react-bootstrap"
 
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -17,26 +18,36 @@ function Header() {
         marginBottom: "0px"
     }
 
+    const homeLink = {
+        color: "white",
+        textDecoration: "none",
+        display: "flex",
+    }
+
     return <div className="header-container">
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" id="header-navbar">
             <div style={navbarTitle}>
-                <img src="adamrossbenton_transparent.png" alt="" id="navbar-logo"/>
-                <div>
-                    <p style={arb}>adam</p>
-                    <p style={arb}>ross</p>
-                    <p style={arb}>benton</p>
-                </div>
+                <Link to="/" style={homeLink}>
+                    <img src="adamrossbenton_transparent.png" alt="" id="navbar-logo"/>
+                    <div>
+                        <p style={arb}>adam</p>
+                        <p style={arb}>ross</p>
+                        <p style={arb}>benton</p>
+                    </div>
+                </Link>
             </div>
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                     <NavDropdown title="Projects">
                         <NavDropdown.Item href ="/projects">View All</NavDropdown.Item>
-                        <NavDropdown.Item href="">TRICKS: THE MEETING</NavDropdown.Item>
-                        <NavDropdown.Item href="">halfprice</NavDropdown.Item>
-                        <NavDropdown.Item href="">chowdr</NavDropdown.Item>
-                        <NavDropdown.Item href="">MINOTAUR</NavDropdown.Item>
-                        <NavDropdown.Item href="">AVARICE the Smith</NavDropdown.Item>
+                        {/* I probably shouldn't hardcode these urls but whatevs */}
+                        {/* ¯\_(ツ)_/¯ */}
+                        <NavDropdown.Item href="/projects/1">AVARICE The Smith</NavDropdown.Item>
+                        <NavDropdown.Item href="/projects/2">Minotaur</NavDropdown.Item>
+                        <NavDropdown.Item href="/projects/3">chowdr</NavDropdown.Item>
+                        <NavDropdown.Item href="/projects/4">halfprice</NavDropdown.Item>
+                        <NavDropdown.Item href="/projects/5">TRICKS: THE MEETING</NavDropdown.Item>
                     </NavDropdown>
                     <Nav.Link>About</Nav.Link>
                     <Nav.Link>Contact</Nav.Link>
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,9 +1,28 @@
-import React from "react"
+import React, {useEffect} from "react"
+import {Link, useHistory} from "react-router-dom"
 
-function Show() {
-    return <>
-        <h1>Hello Show</h1>
-    </>
+function Show(props) {
+    
+    useEffect(() => props.getWorks(), [])
+
+    const history = useHistory()
+    const id = props.match.params.id
+    const works = props.works
+    const work = works?.find(w => {
+        return w.id == id
+    })
+
+    const loaded = () => {
+        return <div>
+            <img src={work.image} alt="url goes here"/>
+            <a href={work.link}><h1>{work.name}</h1></a>
+            <h3>{work.tagline}</h3>
+            <p>{work.description}</p>
+            <h5>{work.techs}</h5>
+        </div>
+    }
+
+    return works? loaded() : props.loading()
 }
 
 export default Show
